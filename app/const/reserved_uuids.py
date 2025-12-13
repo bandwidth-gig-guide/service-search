73297ce52c26ea1887e3541d9bb5464c17c44368
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-12-13 12:43:59 +1100

    feat: add reserved id for `DJ Set`

--- app/const/reserved_uuids.py
+++ app/const/reserved_uuids.py
@@ -1,5 +1,6 @@
 RESERVED_UUIDS_ARRAY = [
     "00000000-0000-0000-0000-000000000000", # Special Guests - Placeholder entry used when the lineup for an event has not been finalized. - Page Type: artist
+    "00000000-0000-0000-0000-000000000001", # DJ Set - Placeholder entry used when the event is just undefined djs. - Page Type: artist
 ]
 
 RESERVED_UUIDS_STRING = "'" + "', '".join(RESERVED_UUIDS_ARRAY) + "'"
